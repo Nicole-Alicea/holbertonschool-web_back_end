cfe3974fd2b84d672e54181c731fabe030aa4392
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-22 15:30:43 -0400

    task 1

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+
+def concat(str1: str, str2: str):
+    '''Type-annotated function that takes a string (str1) and a
+    string (str2) as arguments and returns a concatenated string'''
+
+    result: str
+    result = str1 + str2
+
+    return result
