fe9dc66937866560bec11206b3cc8114dffe03e5
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-22 18:47:07 -0400

    task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+import math
+
+
+def floor(n: float):
+    '''Type-annotated function that takes a float as argument and
+    returns the floor of the float'''
+
+    return math.floor(n)
