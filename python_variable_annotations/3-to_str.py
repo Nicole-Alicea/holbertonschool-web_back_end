c8704240f4a2968d3b6dc4d233f40fea002105b5
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-22 18:58:05 -0400

    task 3

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+
+def to_str(n: float):
+    '''Type-annotated function that takes a float (n) as argument
+    and returns the string representation of the float'''
+
+    return str(n)
