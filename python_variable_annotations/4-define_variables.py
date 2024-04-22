4609c7f6e93486b2e22fd8b146a2c15e354000ea
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-22 19:39:38 -0400

    task 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+'''Will define and annotate the following variables'''
+
+
+def variable_annotations():
+    '''Defining and annotating the following variables with the
+    specified values'''
+
+    a: int
+    pi: float
+    i_understand_annotations: bool
+    school: str
+
+    a = 1
+    pi = 3.14
+    i_understand_annotations = True
+    school = "Holberton"
