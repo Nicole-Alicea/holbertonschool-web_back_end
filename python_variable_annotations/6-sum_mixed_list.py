0482c01b92229fac00a358ac16d17ded567689e6
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-23 14:36:03 -0400

    task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+from typing import List
+
+
+Vector = List[int, float]
+
+
+def sum_mixed_list(mxd_lst: Vector) -> float:
+    '''Type-annotated function that takes a mixed list of integers and
+    floats as argument and returns their sum as a float'''
+
+    return float(sum(mxd_lst))
