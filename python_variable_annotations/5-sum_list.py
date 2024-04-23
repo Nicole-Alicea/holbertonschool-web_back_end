a19fc2c92baa5a4f72b928e95572b53f5ddcf7d3
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-23 13:54:31 -0400

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''Type-annotated function that takes a list of floats as argument
+    and returns their sum as a float'''
+
+    return sum(input_list)
