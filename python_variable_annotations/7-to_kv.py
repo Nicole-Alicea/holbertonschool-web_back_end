a800009391f3c58a63aa844c772033e1aea5f47f
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-23 17:25:05 -0400

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,9 +2,10 @@
 '''The following is a type-annotated function'''
 
 import math
+from typing import Union
 
 
-def to_kv(k: str, v: int | float) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> tuple:
     '''Type-annotated function that takes a string and an integer or float
     (v) as arguments and returns a tuple. The first element of the tuple is
     the string and the second element is the square of v.'''
