38d844f1672da2951e8fe18497aa80d17a0399e9
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-23 17:18:44 -0400

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+import math
+
+
+def to_kv(k: str, v: int | float) -> tuple:
+    '''Type-annotated function that takes a string and an integer or float
+    (v) as arguments and returns a tuple. The first element of the tuple is
+    the string and the second element is the square of v.'''
+
+    square_v = math.sqrt(v)
+
+    return (k, float(square_v))
