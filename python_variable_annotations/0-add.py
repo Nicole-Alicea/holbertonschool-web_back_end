04da0fbb2d1561a0f6d3d5f3a82c5e25172ca833
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-23 12:12:22 -0400

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,4 +6,4 @@ def add(a: float, b: float) -> float:
     '''Type-annotated function that takes a float (a) and a float (b)
     as arguments and returns their sum as a float'''
 
-    return a + b
+    return sum(a + b)
