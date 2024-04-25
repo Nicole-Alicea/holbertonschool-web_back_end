5c487b5e3dc2d50091f237f96a43e9ba8e6bb2d8
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-24 20:05:54 -0400

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+'''The following is a type-annotated function'''
+
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    return [(i, len(i)) for i in lst]
