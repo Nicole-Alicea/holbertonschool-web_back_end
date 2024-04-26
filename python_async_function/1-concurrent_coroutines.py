bade021b7b67895ffbe7c9518ce0c87fe3eb0d94
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-26 16:21:23 -0400

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''The following is an async routine'''
+
+import asyncio
+from typing import List
+from python_async_function import wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Async routine that takes in 2 integer arguments and will spawn
+    wait_random n times with the specified max_delay. Will return the list
+    of all the delays in ascending order without using sort() because of
+    concurrency.'''
+
+    delay = [wait_random(max_delay) for _ in range(n)]
+    all_delays = await asyncio.gather(*delay)
+
+    return sorted(all_delays)
