4e44c0bf2ee211fe8cf3801c84379b9e880aed75
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-04-27 11:11:52 -0400

    task 1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,11 +12,6 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     of all the delays in ascending order without using sort() because of
     concurrency.'''
 
-    delays = []
-
-    tasks = [wait_random(max_delay) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
-    return delays
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = [await task for task in asyncio.as_completed(tasks)]
+    return results
