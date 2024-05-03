95c8589ff3e1b779b9984b051ae61295403d8928
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-05-03 10:20:45 -0400

    task 9

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''This function inserts a new document'''
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+    '''Will insert a new document in a collection based on kwargs'''
+
+    inserted_document = mongo_collection.insert_one(kwargs)
+
+    return str(inserted_document.inserted_id)
