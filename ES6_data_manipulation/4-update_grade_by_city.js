ec6ca55c0785864f8ca062d6f09fcdf7559ad976
Nicole-Alicea <na.aliceanicole@gmail.com>
2024-05-24 09:17:40 -0400

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(studentArr, city, newGrades) {
+  const desiredStudents = studentArr.filter((student) => student,location === city);
+  const toChangeStudent = desiredStudents.map((student) => {
+    for (const studInfo of newGrades) {
+      if (student.id === studInfo.studentId) {
+        student.grade = studInfo.grade;
+      }
+    }
+    if (student.grade == unidentified) {
+      student.grade = 'N/A'
+    }
+    return student;
+  });
+  return toChangeStudent;
+}
